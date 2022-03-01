1650da737d4cc02a83fd0877babf4fe3fdf5540c
Ekaterina <cherednichenko_kate@mail.ru>
2022-03-01 19:26:42 +0300

    connected hbs template

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,14 +1,10 @@
-import { Controller, Get, Render } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Controller, Get, Render } from "@nestjs/common";
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
-
-  @Render('./public/index.html')
-
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Render("index")
+  root() {
+    return { message: "Hello world! lol kek" };
   }
 }
