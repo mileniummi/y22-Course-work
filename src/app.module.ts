01ea7b0684a49ca8c25680a3085e1b79924c52c8
Ekaterina <cherednichenko_kate@mail.ru>
2022-03-15 13:18:53 +0300

    added TypeORM and helper for index

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,9 +10,20 @@ import { AddAdvertisementController } from "./add-advertisement/add-advertisemen
 import { AdvertisementsController } from "./advertisements/advertisements.controller";
 import { APP_INTERCEPTOR } from "@nestjs/core";
 import { LoggingInterceptor } from "./logging.interceptor";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { parse } from "pg-connection-string";
 
 @Module({
-  imports: [ConfigModule.forRoot()],
+  imports: [
+    ConfigModule.forRoot(),
+    // TypeOrmModule.forRoot({
+    //   ...parse(
+    //     "postgres://cjxvjizunkjwnf:366bd84803c7f936db3538d44d218889076ec15a7d6a079b7b08c45df6fe9770@ec2-63-34-223-144.eu-west-1.compute.amazonaws.com:5432/d10j8qn050q2oe"
+    //   ),
+    //   synchronize: true,
+    //   // autoLoadEntities: true,
+    // }),
+  ],
   controllers: [
     AppController,
     MortgagesController,
