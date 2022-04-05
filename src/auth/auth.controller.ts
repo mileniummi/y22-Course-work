aec43b223f6437d8ac56a74a0f64b44241f1bbb1
Mihail <kokosh2000@mail.ru>
2022-04-05 13:19:35 +0300

    added swagger documentation for auth favourites and mortgages endpoints

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,11 +1,49 @@
-import { Body, Controller, Get, Post, Render } from "@nestjs/common";
+import {
+  Body,
+  Controller,
+  Get,
+  NotImplementedException,
+  Post,
+  Render,
+} from "@nestjs/common";
+import { ApiOperation, ApiResponse, ApiTags } from "@nestjs/swagger";
 
+@ApiTags("Authorization")
 @Controller("auth")
 export class AuthController {
-  @Get("/")
+  @ApiOperation({ summary: "Get login page" })
+  @ApiResponse({ status: 200, type: "text/html" })
+  @Get("/login")
   @Render("pages/login")
   getLoginPage() {}
 
-  @Post("/")
-  getUser(@Body() user) {}
+  @ApiOperation({ summary: "Get register page" })
+  @ApiResponse({ status: 200, type: "text/html" })
+  @Get("/register")
+  //@Render("pages/register")
+  getRegisterPage() {
+    throw new NotImplementedException();
+  }
+
+  @ApiOperation({ summary: "Log in system" })
+  @ApiResponse({ status: 200, description: "Returns user token" })
+  @ApiResponse({
+    status: 401,
+    description: "Username or password not correct",
+  })
+  @Post("/login")
+  login(@Body() user) {
+    throw new NotImplementedException();
+  }
+
+  @ApiOperation({ summary: "Register in system" })
+  @ApiResponse({ status: 200, description: "Returns user token" })
+  @ApiResponse({
+    status: 401,
+    description: "User with this username already exists",
+  })
+  @Post("/register")
+  register(@Body() user) {
+    throw new NotImplementedException();
+  }
 }
--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,29 +1,44 @@
 import { Entity, Column, PrimaryGeneratedColumn, OneToMany } from "typeorm";
 import { Advertisement } from "../../advertisements/entities/advertisement.entity";
+import { ApiProperty } from "@nestjs/swagger";
 
 @Entity()
 export class User {
+  @ApiProperty({ example: 1, description: "Unique id of user" })
   @PrimaryGeneratedColumn()
   id: number;
 
+  @ApiProperty({ example: "cat", description: "Username of user" })
   @Column({ unique: true })
   username: string;
 
+  @ApiProperty({ example: "Nikolay", description: "Name of user" })
   @Column()
   firstName: string;
 
+  @ApiProperty({ example: "Andreev", description: "Surname of user" })
   @Column()
   lastName: string;
 
+  @ApiProperty({ example: "querty123", description: "Password of user" })
   @Column()
   password: string;
 
+  @ApiProperty({ example: true, description: "Is user blocked or not" })
   @Column({ default: true })
   isActive: boolean;
 
+  @ApiProperty({
+    example: [Advertisement],
+    description: "All user advertisements",
+  })
   @OneToMany(() => Advertisement, (adv) => adv.author)
   advertisements: Advertisement[];
 
+  @ApiProperty({
+    example: [1, 2, 6],
+    description: "All favourite advertisements id list",
+  })
   @Column("int", { array: true })
   favAdvsID: number[];
 }
