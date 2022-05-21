d75cfa6c752dd359afadb6be6d485f7551ce2ea3
Mihail <kokosh2000@mail.ru>
2022-05-21 16:42:14 +0300

    Revert "just heroku things"
    This reverts commit 4ccba99290ce921ebf7ca988f517279225c6ba4c.

--- src/main.ts
+++ src/main.ts
@@ -65,7 +65,7 @@ async function bootstrap() {
     })
   );
 
-  app.useGlobalInterceptors(new ServerLoadingTimeInterceptor());
+  app.useGlobalInterceptors(new ServerLoadingTimeInterceptor(), new TooMayRequestsInterceptor());
   app.use(requestIp.mw());
   app.useGlobalFilters(new UnauthFilter(), new PageNotFoundFilter(), new ForbiddenFilter());
 
